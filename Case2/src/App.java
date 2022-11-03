a0101c821a65fa83dcd712ab214ff7b95884c975
Kelvin <kelvingiovannno@gmail.com>
2022-11-03 13:11:02 +0700

    Finished | 3/11/22 | 1:10

--- Case2/src/App.java
+++ Case2/src/App.java
@@ -1,14 +1,16 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 class student{
     final String name;
     final int age;
-    protected float gpa;
+    protected ArrayList<Float> gpa = new ArrayList<>();
 
-    student(String name, int age, Float gpa){
+    student(String name, int age, float gpa){
         this.name = name;
         this.age = age;
-        this.gpa = gpa;
+        this.gpa.add(gpa);
     }
 
     public String getName(){
@@ -19,12 +21,19 @@ class student{
         return this.age;
     }
 
-    public float getGpa(){
-        return this.gpa;
+    public void getGpa(){
+        for(int i = 0; i < gpa.size(); i++){
+            if(i == 0){
+                System.out.print(gpa.get(i));
+            }
+            else{
+                System.out.print(" / " + gpa.get(i));
+            }
+        }
     }
 
-    public void setData(String name, float gpa){
-        this.gpa = gpa;
+    public void setData(float gpa){
+        this.gpa.add(gpa);
     }
 
 }
@@ -34,35 +43,65 @@ public class App {
     static Scanner scanf = new Scanner(System.in);
     static student newStudent;
     public static void main(String[] args) throws Exception {
+        insertData();
+        cls(args);
+
         while(true){
-            insertData();
+            menu();
             switch(op()){
                 case 1:
-                    viewData();
+                    cls(args);
+                    viewData(0);
                     break;
                 case 2:
+                    cls(args);
+                    updateData();
                     break;
                 case 3:
-                System.exit(0);
+                    cls(args);
+                    System.exit(0);
                     break;
             }
+            cls(args);
         }
     }
 
     public static void menu(){
-        System.out.println("Wellcom ");
+        System.out.println("Wellcom " + newStudent.getName());
         System.out.println("Student Dummy Data");
         System.out.println("==================");
         System.out.println("1. View Data");
         System.out.println("2. Update Data");
         System.out.println("3. Exit");
-        System.out.println(" >> ");
+        System.out.print(" >> ");
     }
 
-    public static void viewData(){
+    public static void viewData(int flag){
         System.out.println("Your name is " + newStudent.getName());
         System.out.println("Your age is " + newStudent.getAge());
-        System.out.println("yout gpa is " + newStudent.getGpa());
+        System.out.print("yout gpa is "); 
+        newStudent.getGpa();
+        if(flag == 0){
+            System.out.println("\npress (Enter) to back to menu ..");
+            scanf.nextLine();
+        }
+    }
+
+    public static void updateData(){
+        float gpa;
+        viewData(1);
+        System.out.println("\n");
+        do{
+            System.out.print("Input yout gpa [0.0 - 4.0 inclusive]: ");
+            gpa = scanf.nextFloat();
+            scanf.nextLine();
+        }
+        while(gpa < 0 || gpa > 4);
+
+        newStudent.setData(gpa);
+        System.out.println("Student data updated successfully");
+        System.out.println("\npress (Enter) to back to menu ..");
+        scanf.nextLine();
     }
 
     public static void insertData(){
@@ -86,7 +125,7 @@ public class App {
         while(age < 16);
 
         do{
-            System.out.print("Input yout gpa [0.0 - 4.0 inclusive]: -1");
+            System.out.print("Input yout gpa [0.0 - 4.0 inclusive]: ");
             gpa = scanf.nextFloat();
             scanf.nextLine();
         }
@@ -101,5 +140,7 @@ public class App {
         return op;
     }
 
-
+    public static void cls (String... arg) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
 }
